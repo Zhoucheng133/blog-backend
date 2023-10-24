738c31944ad6dd38b689371eac43b41d77b2e2bf
zhoucheng <zhoucheng133@outlook.com>
2023-10-24 18:38:33 +0800

    修复获取类别的问题

--- src/main/java/blog/backend/blog/MariaDB/operations.java
+++ src/main/java/blog/backend/blog/MariaDB/operations.java
@@ -209,15 +209,17 @@ public class operations {
         String title="";
         String tag=null;
         Timestamp date=null;
+        String cata="";
         try {
             Class.forName(DRIVER);
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT title, createDate, tag FROM blog WHERE id = " + id);
+            ResultSet resultSet = statement.executeQuery("SELECT title, createDate, tag, cata FROM blog WHERE id = " + id);
             if (resultSet.next()) {
                 title = resultSet.getString(1);
                 date=resultSet.getTimestamp(2);
                 tag = resultSet.getString(3);
+                cata = resultSet.getString(4);
                 if (title == null || title.equals("null")) {
                     return new blogContent("", null, null, null);
                 }
@@ -225,7 +227,7 @@ public class operations {
             resultSet.close();
             statement.close();
             connection.close();
-            return new blogContent(title, tag, date, null);
+            return new blogContent(title, tag, date, cata);
         } catch (Exception e) {
             System.err.println("错误: " + e.getMessage());
         }
